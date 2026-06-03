d947656abd17d48f2c019732f271c506d54d07fc
wdrf14 <weidermartenn@gmail.com>
2026-06-03 15:00:39 +0300

    fixed imports

--- backend/src/messages/messages.module.ts
+++ backend/src/messages/messages.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { MessagesService } from './messages.service';
 import { MessagesController } from './messages.controller';
+import { AuthModule } from '@/auth';
 
 @Module({
   controllers: [MessagesController],
   providers: [MessagesService],
+  imports: [AuthModule],
 })
 export class MessagesModule {}
